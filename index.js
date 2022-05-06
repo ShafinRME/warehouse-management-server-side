2f8721c3e79e62db2a3db60b59b5336a4b71a6aa
ShafinRME <shafin.rmedu4@gmail.com>
2022-05-07 00:11:07 +0600

    New database created and data is loaded and connected with server

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-
+// Pass: eQU8kt5khRTkKwgQ
+// User: SpaceX-Car-House
 // Declaring and Using of MiddleWare
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@spacex-car-house.t231t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('He he connected bro connected');
+    // perform actions on the collection object
+    client.close();
+});
+
+
 app.get('/', (req, res) => {
-    res.send('SpaceX Car Inventory is running successfully!!!!');
+    res.send('SpaceX Car House is running successfully!!!!');
 });
 
 app.listen(port, () => {
